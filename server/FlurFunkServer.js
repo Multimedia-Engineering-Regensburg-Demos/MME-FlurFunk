123a1cfda51db3aa06bd203927f6dfb8126eebc3
Alexander Bazo <alexanderbazo@googlemail.com>
2021-01-13 11:11:44 +0100

    Remove unused function

--- server/FlurFunkServer.js
+++ server/FlurFunkServer.js
@@ -4,15 +4,7 @@ import { ServerMessage, BroadcastMessage } from "../shared/Message.js";
 var ws,
     messages = [];
 
-function getRandomColor() {
-    let r = (255 + Math.random() * 256) / 2;
-    let g = (255 + Math.random() * 256) / 2;
-    let b = (255 + Math.random() * 256) / 2;
-    return `rgb(${r},${g},${b})`;
-}
-
 function onClientConnect(socket) {
-    socket.clientColor = getRandomColor();
     socket.on("message", onClientMessage.bind(socket));
     socket.emit("history", new ServerMessage(socket.id, messages));
 }
